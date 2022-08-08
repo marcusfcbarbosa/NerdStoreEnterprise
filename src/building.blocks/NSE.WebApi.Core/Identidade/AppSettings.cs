3047a24f6f4cf2ca10aa4af392fde3000bd3955d
marcusfcbarbosa <marcusfernandocbarbosa@gmail.com>
2022-08-08 00:15:38 -0300

    WIP

---
+++ src/building.blocks/NSE.WebApi.Core/Identidade/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace NSE.WebApi.Core.Identidade
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; }
+        public string ValidoEm { get; set; }
+    }
+}
--- src/services/NSE.Identity.API/Configuration/IdentityConfig.cs
+++ src/services/NSE.Identity.API/Configuration/IdentityConfig.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using NSE.Identity.API.Data;
 using NSE.Identity.API.Extensions;
+using NSE.WebApi.Core.Identidade;
 using System.Text;
 
 namespace NSE.Identity.API.Configuration
@@ -24,42 +25,10 @@ namespace NSE.Identity.API.Configuration
                 .AddErrorDescriber<IdentitysMensagensPortugues>()//Customizando para exibir mensagens em Português
                 .AddEntityFrameworkStores<ApplicationDBContext>()
                 .AddDefaultTokenProviders();
-
-
-            // JWT
-            var appSettingsSection = configuration.GetSection("AppSettings");
-            services.Configure<AppSettings>(appSettingsSection);
-
-            var appSettings = appSettingsSection.Get<AppSettings>();
-            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(bearerOptions =>
-            {
-                bearerOptions.RequireHttpsMetadata = true;
-                bearerOptions.SaveToken = true;
-                bearerOptions.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = true,//sempre validar o emissor do token
-                    ValidateAudience = true,//onde esse token é valido
-                    ValidAudience = appSettings.ValidoEm,//audiencia valida
-                    ValidIssuer = appSettings.Emissor //emissor valido (onde esse token sera valido)
-                };
-            });
+            services.AddJWTConfiguration(configuration);
             return services;
         }
 
-        public static IApplicationBuilder UseIdentityConfiguration(this IApplicationBuilder app)
-        {
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            return app;
-        }
+        
     }
 }
