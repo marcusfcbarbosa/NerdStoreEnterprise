e35497cce1a272251f8595147d5be166385c9226
marcusfcbarbosa <marcusfernandocbarbosa@gmail.com>
2022-08-07 23:43:28 -0300

    API Funcionando de catalog com as configurações basicas

---
+++ src/services/NSE.Catalogo.API/Configuration/ApiConfig.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NSE.Catalogo.API.Data;
+
+namespace NSE.Catalogo.API.Configuration
+{
+    public static  class ApiConfig
+    {
+        public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<CatalogoContext>(optionsAction: options =>
+            options.UseSqlServer(configuration.GetConnectionString(name: "DefaultConnection")));
+            services.AddControllers();
+            //Como sera chamada por varias apis, habilitar o CORS
+
+            services.AddCors(options => {
+                options.AddPolicy(name:"Acesso_Total",
+                    configurePolicy: builder =>
+                    builder
+                    .AllowAnyHeader()
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod());
+            });
+        }
+
+
+        public static void UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
--- src/web/NSE.WebApp.MVC/Services/IAutenticacaoService.cs
+++ src/web/NSE.WebApp.MVC/Services/IAutenticacaoService.cs
@@ -11,8 +11,8 @@ namespace NSE.WebApp.MVC.Services
 {
     public interface IAutenticacaoService
     {
-        Task<T> Login<T>(UsuarioLogin usuarioLogin) where T : class;//Precisa pensar num formato para ter retorno das mensagens em Generic
-        Task<T> Registro<T>(UsuarioRegistro usuarioRegistro) where T : class;//Precisa pensar num formato para ter retorno das mensagens em Generic
+        //Task<T> Login<T>(UsuarioLogin usuarioLogin) where T : class;//Precisa pensar num formato para ter retorno das mensagens em Generic
+        //Task<T> Registro<T>(UsuarioRegistro usuarioRegistro) where T : class;//Precisa pensar num formato para ter retorno das mensagens em Generic
         Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin);
         Task<UsuarioRespostaLogin> Registro(UsuarioRegistro usuarioRegistro);
     }
@@ -27,25 +27,25 @@ namespace NSE.WebApp.MVC.Services
             _httpClient.BaseAddress = new Uri(settings.Value.AutenticacaoUrl);
             
         }
-        public async Task<T> Login<T>(UsuarioLogin usuarioLogin) where T : class
-        {
-            try
-            {
-                var loginContent = ObterConteudo(usuarioLogin);
-                var response = await _httpClient.PostAsync(requestUri: "https://localhost:44307/api/identidade/autenticar", content: loginContent);
-                var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-                if (!TratarErrosResponse(response))
-                {
-                    //Nesse formato ainda nao pega as mensagens de retorno
-                }
-                return JsonSerializer.Deserialize<T>(await response.Content.ReadAsStringAsync(), options);
-            }
-            catch (Exception ex)
-            {
-                //TO DO capturar esse erro em log
-                return default(T);
-            }
-        }
+        //public async Task<T> Login<T>(UsuarioLogin usuarioLogin) where T : class
+        //{
+        //    try
+        //    {
+        //        var loginContent = ObterConteudo(usuarioLogin);
+        //        var response = await _httpClient.PostAsync(requestUri: "https://localhost:44307/api/identidade/autenticar", content: loginContent);
+        //        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        //        if (!TratarErrosResponse(response))
+        //        {
+        //            //Nesse formato ainda nao pega as mensagens de retorno
+        //        }
+        //        return JsonSerializer.Deserialize<T>(await response.Content.ReadAsStringAsync(), options);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        //TO DO capturar esse erro em log
+        //        return default(T);
+        //    }
+        //}
         public async Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin)
         {
             var loginContent = ObterConteudo(usuarioLogin);
