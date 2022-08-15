bc04e3def98594c1bd5dd6e9080b4c8c03d4a222
marcusfcbarbosa <marcusfernandocbarbosa@gmail.com>
2022-08-14 21:22:35 -0300

    Estrututra de validação pronta

--- src/services/NSE.Cliente.API/Application/Commands/RegistrarClientCommand.cs
+++ src/services/NSE.Cliente.API/Application/Commands/RegistrarClientCommand.cs
@@ -1,10 +1,15 @@
-﻿using NSE.Core.Messages;
+﻿using FluentValidation;
+using NSE.Core.Messages;
 using System;
 
 namespace NSE.Cliente.API.Application.Commands
 {
     public class RegistrarClientCommand : Command
     {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+
         public RegistrarClientCommand(string nome, string email, string cpf)
         {
             AggregateId = Guid.NewGuid();
@@ -12,9 +17,36 @@ namespace NSE.Cliente.API.Application.Commands
             Email = email;
             Cpf = cpf;
         }
+        public override bool EhValido()
+        {
+            validationResult = new RegistrarClientValidation().Validate(instance:this);
+            return validationResult.IsValid;
+        }
+    }
 
-        public string Nome { get; private set; }
-        public string Email { get; private set; }
-        public string Cpf { get; private set; }
+    public class RegistrarClientValidation : AbstractValidator<RegistrarClientCommand>
+    {
+        public RegistrarClientValidation()
+        {
+            RuleFor(c => c.Nome)
+                .NotEqual(string.Empty)
+                .WithMessage("Nome Inválido");
+            
+            RuleFor(c => c.Cpf)
+                .Must(CpfValido)
+                .WithMessage("Cpf Inválido");
+
+            RuleFor(c => c.Email)
+                .Must(EmailValido)
+                .WithMessage("EmailValido Inválido");
+        }
+        protected static bool CpfValido(string cpf)
+        {
+            return Core.DomainObjects.Cpf.Validar(cpf);
+        }
+        protected static bool EmailValido(string email)
+        {
+            return Core.DomainObjects.Email.Validar(email);
+        }
     }
 }
