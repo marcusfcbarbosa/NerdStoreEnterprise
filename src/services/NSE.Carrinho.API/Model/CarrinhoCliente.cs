c4a381ea4c23f539652c4334742a944da452bfe7
marcusfcbarbosa <marcusfernandocbarbosa@gmail.com>
2022-08-21 23:03:33 -0300

    Estrutura inicial do Carrinho mapeada

---
+++ src/services/NSE.Carrinho.API/Model/CarrinhoCliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NSE.Carrinho.API.Model
+{
+    public class CarrinhoCliente
+    {
+
+        public Guid Id { get; set; }
+        public Guid ClienteId { get; set; }
+        public decimal ValorTotal { get; set; }
+        public List<CarrinhoItem> Itens { get; set; } = new List<CarrinhoItem>();
+        protected CarrinhoCliente() { }
+        public CarrinhoCliente(Guid ClienteId)
+        {
+            Id = Guid.NewGuid();
+            ClienteId = Guid.NewGuid();
+        }
+    }
+}
