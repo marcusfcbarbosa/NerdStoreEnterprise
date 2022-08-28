680c54d6d626fd3c6ede268b1f100f67f059f897
marcusfcbarbosa <marcusfernandocbarbosa@gmail.com>
2022-08-27 21:35:57 -0300

    Criando os servicos e o DelegateHandler

---
+++ src/api.gateway/NSE.BFF.Compras/Controllers/CarrinhoController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApi.Core.Controllers;
+using System.Threading.Tasks;
+
+namespace NSE.BFF.Compras.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarrinhoController : MainController
+    {
+        [HttpGet("compras/carrinho")]
+        public async Task<IActionResult> Index()
+        {
+            return CustomResponse();
+        }
+
+        [HttpGet]
+        [Route("compras/carrinho-quantidade")]
+        public async Task<IActionResult> ObterQuantidadeCarrinho()
+        {
+            return CustomResponse();
+        }
+        
+        [HttpPost]
+        [Route("compras/carrinho/item")]
+        public async Task<IActionResult> AdicionarItemCarrinho()
+        {
+            return CustomResponse();
+        }
+        
+        [HttpPut]
+        [Route("compras/carrinho/items/{produtoId}")]
+        public async Task<IActionResult> AtualizarItemCarrinho()
+        {
+            return CustomResponse();
+        }
+
+        [HttpDelete]
+        [Route("compras/carrinho/items/{produtoId}")]
+        public async Task<IActionResult> DeletarItemCarrinho()
+        {
+            return CustomResponse();
+        }
+
+    }
+}
