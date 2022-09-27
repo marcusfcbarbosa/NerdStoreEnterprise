7c8f0324be72e455d929261b756239112c1b56ee
marcusfcbarbosa <marcusfernandocbarbosa@gmail.com>
2022-09-27 13:55:05 -0300

    Paginação na camada MVC, porém ainda nao funciona

--- src/web/NSE.WebApp.MVC/Controllers/CatalogoController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/CatalogoController.cs
@@ -18,9 +18,10 @@ namespace NSE.WebApp.MVC.Controllers
         [HttpGet]
         [Route("")]
         [Route("vitrine")]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index([FromQuery] int ps = 8, [FromQuery] int page = 1, [FromQuery] string q = null)
         {
-            var produtos = await _catalogoService.ObterTodos();
+            var produtos = await _catalogoService.ObterTodosPaginado(ps, page, q);
+            ViewBag.Pesquisa = q;
             return View(produtos);
         }
 
